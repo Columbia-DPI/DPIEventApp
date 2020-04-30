31972bcc9e64e2a5a53f0395d01dd684da20052f
AbbyLuHui <hl3231@columbia.edu>
2020-04-30 14:31:06 +0800

    initialize event component

--- react_frontend/src/pages/home.jsx
+++ react_frontend/src/pages/home.jsx
@@ -1,31 +1,33 @@
 import React, { Component } from "react";
-import AliceCarousel from 'react-alice-carousel'
-import 'react-alice-carousel/lib/alice-carousel.css'
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import Event from '/../components/event.jsx';
 
 class HomePage extends Component {
 
   state = {
     galleryItems: [1, 2, 3].map((i) => <h2 key={i}>{i}</h2>),
   }
- 
+
   responsive = {
     0: { items: 1 },
     1024: { items: 2 },
   }
- 
+
   onSlideChange(e) {
     console.debug('Item`s position during a change: ', e.item)
     console.debug('Slide`s position during a change: ', e.slide)
   }
- 
+
   onSlideChanged(e) {
     console.debug('Item`s position after changes: ', e.item)
     console.debug('Slide`s position after changes: ', e.slide)
   }
- 
+
   render() {
     // javascript code here
     return (
+      <div>
       <AliceCarousel
         items={this.state.galleryItems}
         responsive={this.responsive}
@@ -39,10 +41,10 @@ class HomePage extends Component {
         onSlideChange={this.onSlideChange}
         onSlideChanged={this.onSlideChanged}
       />
+      <Event info={{title:"event"}} />
+      </div>
     )
   }
 }
 
 export default HomePage;
-
-
