428698be6016afaa24821d106a6c74d5e587c278
Portia Wang <60831440+pwang1230@users.noreply.github.com>
2020-05-01 01:25:48 +0800

    implemented carousel on home page

---
+++ flask_backend/static/react/precache-manifest.7a49c36d364d3fe99c0267026d2534e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d3fc84a8d3066ded19ff73a372bbde",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "b6f512540e22d0a88941",
+    "url": "/static/react/css/2.27433345.chunk.css"
+  },
+  {
+    "revision": "2d3ac5d0c3079adc956a",
+    "url": "/static/react/css/main.cb8d77ee.chunk.css"
+  },
+  {
+    "revision": "b6f512540e22d0a88941",
+    "url": "/static/react/js/2.2331a8e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/react/js/2.2331a8e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3ac5d0c3079adc956a",
+    "url": "/static/react/js/main.7179ab28.chunk.js"
+  },
+  {
+    "revision": "378e2c10579bd9ac7f95",
+    "url": "/static/react/js/runtime-main.ab287a6d.js"
+  },
+  {
+    "revision": "d9d8372820072530df2857765225c6be",
+    "url": "/static/react/media/img_1.d9d83728.jpg"
+  },
+  {
+    "revision": "981d091e4105167f1ba16d3b47b0d6c4",
+    "url": "/static/react/media/img_2.981d091e.jpg"
+  },
+  {
+    "revision": "0893d01e2ecf6856d95ba5cbfe35e664",
+    "url": "/static/react/media/img_3.0893d01e.jpg"
+  },
+  {
+    "revision": "e004cf8293e5e163116de887d4a7e74a",
+    "url": "/static/react/media/img_4.e004cf82.jpg"
+  },
+  {
+    "revision": "d0aa40386ff3217c42f4ab4e92a581fe",
+    "url": "/static/react/media/img_5.d0aa4038.jpg"
+  },
+  {
+    "revision": "33feaf225b221c28bf3c720a960d3ff6",
+    "url": "/static/react/media/img_6.33feaf22.jpg"
+  }
+]);
