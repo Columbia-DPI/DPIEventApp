ce21990357f1638fd0409684bdd27c52128f28ee
Sara <yz3504@columbia.edu>
2020-05-04 11:03:33 +0800

    Added queries

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -1,15 +1,32 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Response, jsonify
+"""
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+from user import User
+from dotenv import load_dotenv
+"""
+import requests
+import os
+import sys
+import logging
+from db import Db
+#load_dotenv()
+
+# database connection
+sess_db = Db.init_db()
+
 
 flask_backend = Flask(__name__)
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 flask_backend.config['TEMPLATES_AUTO_RELOAD'] = True
-
-import dbqueries as db
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 
