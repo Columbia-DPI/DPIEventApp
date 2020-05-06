6de3f541ca2e6db045726e472e9621ec7465ff6a
Mustafa Eyceoz <maxusmusti@gmail.com>
2020-05-06 15:41:01 -0400

    More Stuff

---
+++ flask_backend/static/react/precache-manifest.68366ffe371ead2b518f633590a62775.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4249079e109a2759b332eed99996f16",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "ba9108c3b3e7f27615e8",
+    "url": "/static/react/css/2.b706bf63.chunk.css"
+  },
+  {
+    "revision": "be52ecd655ac9b5853ad",
+    "url": "/static/react/css/main.456629ec.chunk.css"
+  },
+  {
+    "revision": "ba9108c3b3e7f27615e8",
+    "url": "/static/react/js/2.8570fb39.chunk.js"
+  },
+  {
+    "revision": "9f0dd39b504620a945c3ab28c779b2d0",
+    "url": "/static/react/js/2.8570fb39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be52ecd655ac9b5853ad",
+    "url": "/static/react/js/main.5f521f4a.chunk.js"
+  },
+  {
+    "revision": "378e2c10579bd9ac7f95",
+    "url": "/static/react/js/runtime-main.ab287a6d.js"
+  },
+  {
+    "revision": "d9d8372820072530df2857765225c6be",
+    "url": "/static/react/media/img_1.d9d83728.jpg"
+  },
+  {
+    "revision": "981d091e4105167f1ba16d3b47b0d6c4",
+    "url": "/static/react/media/img_2.981d091e.jpg"
+  },
+  {
+    "revision": "0893d01e2ecf6856d95ba5cbfe35e664",
+    "url": "/static/react/media/img_3.0893d01e.jpg"
+  },
+  {
+    "revision": "e004cf8293e5e163116de887d4a7e74a",
+    "url": "/static/react/media/img_4.e004cf82.jpg"
+  },
+  {
+    "revision": "d0aa40386ff3217c42f4ab4e92a581fe",
+    "url": "/static/react/media/img_5.d0aa4038.jpg"
+  },
+  {
+    "revision": "33feaf225b221c28bf3c720a960d3ff6",
+    "url": "/static/react/media/img_6.33feaf22.jpg"
+  },
+  {
+    "revision": "446cc987baad53441e583f920a67b92d",
+    "url": "/static/react/media/profile.446cc987.png"
+  }
+]);
