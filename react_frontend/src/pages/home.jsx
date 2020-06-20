b1df9631e6cd41b384361b5c73e8786b6a56c952
Yuanyuting Wang <yw3241@columbia.edu>
2020-06-19 20:06:42 -0400

    fixed carousel

--- react_frontend/src/pages/home.jsx
+++ react_frontend/src/pages/home.jsx
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import Event from '../containers/event.jsx';
 import Header from '../containers/header.jsx'
 import Collection_cell from '../containers/collection_cell.jsx'
 import Collection from '../containers/collection.jsx'
 import styled from 'styled-components'
 import '../style.css'
+import { Button } from "semantic-ui-react";
+
+
+const responsiveCarousel = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
 
 const CarouselContainer = styled.div`
   width: 100%;
-  height: 20rem;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  display: table;
-  table-layout: fixed;
-  border-spacing: 10px;
-  background: red;
-`
-const ColumnSide = styled.div`
-  display: table-cell; /*Optional*/
-  width: 10%;
-  & :hover {
-    background: lightGray
-  }
-`
-const ColumnCenter = styled.div`
-  display: table-cell;
-  background: blue;
-  width: 80%; 
+  padding: 0 8rem;
+  margin-top: 3rem;
 `
 
 class HomePage extends Component {
@@ -37,30 +40,18 @@ class HomePage extends Component {
     super(props);
     this.state = {
       serverData: null,
-      currentIndex: 0,
-      itemsInSlide: 1,
-      showList: false,
-      responsive: { 0: { items: 3 } },
       events: []
     }
 
     this.fetchAllEvents = this.fetchAllEvents.bind(this)
     this.eventList = this.eventList.bind(this)
-    this.slideNextPage = this.slideNextPage.bind(this)
-    this.slidePrevPage = this.slidePrevPage.bind(this)
-    this.displayAllEvents = this.displayAllEvents.bind(this)
-    this.handleOnSlideChange = this.handleOnSlideChange.bind(this)
-    this.eventList = this.eventList.bind(this)
-    this.printindex = this.printindex.bind(this)
   }
 
   componentDidMount(){
     this.fetchAllEvents()
   }
 
-  componentDidUpdate(){
-    console.log(this.state.currentIndex)
-  }
+  
   fetchAllEvents() {
     let payload={
       "placeholder": "nothing rn",
@@ -77,9 +68,6 @@ class HomePage extends Component {
       })})
   }
 
-  printindex(){
-    console.log(this.state.currentIndex)
-  }
   eventList(type) {
       if (type == "all"){
           return this.state.events
@@ -97,68 +85,31 @@ class HomePage extends Component {
                                                     timestamp: item['timestamp']}}/>)
   }
 
-  slidePrevPage = () => {
-    const currentIndex = Math.max(0, this.state.currentIndex - this.state.itemsInSlide)
-    this.setState({ currentIndex: currentIndex })
-  }
-
-  slideNextPage = () => {
-    let currentIndex = this.state.currentIndex + this.state.itemsInSlide
-    if (currentIndex > this.state.events.length) currentIndex = this.state.events.length
-    this.setState({ currentIndex: currentIndex })
-    console.log("currentIndex: " + currentIndex + ", this.state.currentIndex: " + this.state.currentIndex)
-    
-  }
-
-  handleOnSlideChange = (event) => {
-    this.setState({ currentIndex: event.item })
-    console.log("handleOnSlideChange: event.item=" + event.item + ", currentIndex: " + this.state.currentIndex)
-  }
-
-  displayAllEvents = () =>{
-    this.setState({
-      showList: !this.state.showList
-    })
-  }
-
   render() {
-    var listComp = null
-    var showButton = <button class="showEventButton" onClick={this.displayAllEvents}>show all events</button>
-    const { currentIndex, events, responsive, showList} = this.state
-    if (showList){
-      listComp = this.eventList("all")
-      showButton = <button class="collapseEventButton" onClick={this.displayAllEvents}>collapse list</button>
-    }
-
     return (
       <div>
         <Header/>
-        <button onClick={this.printindex}>print state</button>
         <CarouselContainer>
-          <ColumnSide>
-            <button class="button" onClick={this.slidePrevPage}><i class="i arrow left"></i></button>
-          </ColumnSide>
-          <ColumnCenter>
-            <AliceCarousel
-              items={this.eventList("carousel")}
-              slideToIndex={currentIndex}
-              responsive={responsive}
-              buttonsDisabled={false}
-              onInitialized={this.handleOnSlideChange}
-              onSlideChanged={this.handleOnSlideChange}
-              onResized={this.handleOnSlideChange}
-            />
-          </ColumnCenter>
-          <ColumnSide>
-            <button class="button" onClick={this.slideNextPage}><i class="i arrow right"></i></button>
-          </ColumnSide>
+          <Carousel responsive={responsiveCarousel}
+                    autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                    autoPlaySpeed={3000}
+                    keyBoardControl={true}
+                    removeArrowOnDeviceType={["tablet", "mobile"]}
+                    deviceType={this.props.deviceType}
+                    itemClass="carousel-item-padding-40-px">
+            {this.state.events.map((item, i) => {
+              return(
+                <div key={"div" + i}>
+                  <Event info = {{title: item['title'],
+                                                    type: "Academic",
+                                                    img: item['link'],
+                                                    description: item['description'],
+                                                    timestamp: item['timestamp']}}/>
+                </div>
+              )
+            })}
+          </Carousel>
         </CarouselContainer>
-        <div>
-          {showButton}
-        </div>
-        <div>
-          {listComp}
-        </div>
 
       </div>
     )
